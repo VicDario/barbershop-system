e4e01bfc29548015e19cc4f8068e47bc344d60d2
VicDario <26585356+VicDario@users.noreply.github.com>
2024-11-08 05:36:29 -0300

    feat: add shop repository

---
+++ src/infrastructure/repositories/shop_repository.py
@@ -0,0 +1,8 @@
+from src.db.postgresql import DatabaseInterface
+
+class ShopRepository:
+    def __init__(self, db: DatabaseInterface) -> None:
+        self.db = db
+    
+    def get_by_id(self, id: int):
+        return self.db.fetch_one("SELECT name FROM shop WHERE id=%s;", [id])
