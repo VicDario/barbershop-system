43b248ae42f7372a0f87c5cc83572ec50690b10d
VicDario <26585356+VicDario@users.noreply.github.com>
2024-11-20 02:37:07 -0300

    feat: return more profits details

--- src/infrastructure/repositories/profit_repository.py
+++ src/infrastructure/repositories/profit_repository.py
@@ -12,11 +12,14 @@ class ProfitRepository(ProfitRepositoryInterface):
             WITH profits AS (
                 SELECT 
                     shop_id,
-                    SUM(profit) AS profit
+                    SUM(sales_profit) AS sales_profit,
+                    SUM(services_profit) AS services_profit,
+                    SUM(sales_profit + services_profit) AS profit
                 FROM (
                     SELECT 
                         sv.shop_id,
-                        SUM((p.price - si.discount) * si.number) AS profit
+                        SUM((p.price - si.discount) * si.number) AS sales_profit,
+                        0 as services_profit
                     FROM sales_vouchers sv
                     JOIN sales_in si ON sv.number_voucher = si.number_voucher
                     JOIN products p ON si.product_code = p.code
@@ -27,7 +30,8 @@ class ProfitRepository(ProfitRepositoryInterface):
 
                     SELECT 
                         b.shop_id,
-                        SUM(s.base_price - COALESCE(d.mount, 0)) AS profit
+                        0 as sales_profit,
+                        SUM(s.base_price - COALESCE(d.mount, 0)) AS services_profit
                     FROM bookings b
                     LEFT JOIN payment_documents pd ON b.code = pd.booking_code
                     LEFT JOIN discounts d ON pd.document_number = d.payment_document_number
@@ -41,10 +45,12 @@ class ProfitRepository(ProfitRepositoryInterface):
             SELECT 
                 p.shop_id,
                 sh.name,
+                p.sales_profit,
+                p.services_profit,
                 p.profit
             FROM profits p
             JOIN shops sh ON sh.id = p.shop_id
-            GROUP BY shop_id, name, profit
+            GROUP BY shop_id, name, sales_profit, services_profit, profit
             HAVING profit > (SELECT AVG(profit) FROM profits)
             ORDER BY profit DESC;
         """, (date_param, date_param))
