5bc1335907452fdcfa342c667226c8bae6c1f31d
VicDario <26585356+VicDario@users.noreply.github.com>
2024-11-19 21:54:57 -0300

    refactor: extract print to a private function

--- src/infrastructure/use_cases/lastyear_profits_usecase.py
+++ src/infrastructure/use_cases/lastyear_profits_usecase.py
@@ -10,20 +10,23 @@ class LastYearProfitsUseCase:
         one_year_less = current_date.replace(year=current_date.year - 1)
         sales_profits = self.profit_repository.get_profits_from_sales_since_date(one_year_less)
         services_profits = self.profit_repository.get_profits_from_services_since_date(one_year_less)
-        merged_profits = {}
+        profits_by_shop = {}
 
         for sale in sales_profits:
             shop_id = sale["shop_id"]
-            merged_profits[shop_id] = { "sales_profit": sale["total_profit"], "services_profit": 0 }
+            profits_by_shop[shop_id] = { "sales_profit": sale["total_profit"], "services_profit": 0 }
 
         for service in services_profits:
             shop_id = sale["shop_id"]
-            if shop_id not in merged_profits: merged_profits[shop_id] = { "sales_profit": 0 }
-            merged_profits[shop_id].update({ "services_profit": service["total_profit"] })
+            if shop_id not in profits_by_shop: profits_by_shop[shop_id] = { "sales_profit": 0 }
+            profits_by_shop[shop_id].update({ "services_profit": service["total_profit"] })
 
-        for key, value in merged_profits.items():
+        self.__show_profits_list(profits_by_shop)
+       
+
+    def __show_profits_list(self, profits):
+        for key, value in profits.items():
             sales_profit = value["sales_profit"]
             services_profit = value["services_profit"]
             total = sales_profit + services_profit
             print(f"Id Local: {key}, Ganancias por venta de productos: {sales_profit}, Ganancias por servicios: {services_profit}, Ganancias totales: {total}")
-            
