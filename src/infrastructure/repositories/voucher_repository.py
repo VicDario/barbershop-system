bfe800018b7691fb01fdbd6c9ac738fa794d9979
VicDario <26585356+VicDario@users.noreply.github.com>
2024-11-20 16:23:42 -0300

    feat: improve query and show sales vouchers

--- src/infrastructure/repositories/voucher_repository.py
+++ src/infrastructure/repositories/voucher_repository.py
@@ -8,17 +8,18 @@ class VoucherRepository(VoucherRepositoryInterface):
     def get_sales_vouchers(self):
         return self.db.fetch_all("""
             SELECT 
-                sv.number_voucher, 
-                sv.date, 
-                sv.client_rut, 
-                sv.shop_id, 
-                p.name AS product_name, 
-                si.number AS quantity_sold, 
-                si.discount,
-                s.name AS shop_name
+                sv.number_voucher,
+                sv.date,
+                sv.client_rut,
+                sv.shop_id,
+                s.name AS shop_name,
+                s.community,
+                STRING_AGG(p.name || ' (Cantidad: ' || si.number || ')', '\n') AS products_sold,
+                SUM((p.price * si.number) - si.discount) AS total_price
             FROM sales_vouchers sv
             JOIN sales_in si ON sv.number_voucher = si.number_voucher
             JOIN products p ON si.product_code = p.code
             JOIN shops s ON sv.shop_id = s.id
+            GROUP BY sv.number_voucher, sv.date, sv.client_rut, sv.shop_id, s.name, s.community
             ORDER BY sv.date DESC;
         """)
--- src/infrastructure/use_cases/show_sales_vouchers_by_shop_usecase.py
+++ src/infrastructure/use_cases/show_sales_vouchers_by_shop_usecase.py
@@ -1,3 +1,4 @@
+from tabulate import tabulate
 from src.domain.repositories.voucher_repository import VoucherRepositoryInterface
 
 class ShowSalesVouchersByShop:
@@ -6,4 +7,6 @@ class ShowSalesVouchersByShop:
 
     def execute(self):
         vouchers = self.repository.get_sales_vouchers()
-        print(vouchers)
+        headers = ["Número documento de venta", "Fecha", "RUT cliente", "ID tienda", "Nombre tienda", "Municipalidad", "Productos", "Precio total"]
+        rows = [[voucher["number_voucher"], voucher["date"], voucher["client_rut"], voucher["shop_id"], voucher["shop_name"], voucher["community"], voucher["products_sold"], voucher["total_price"]] for voucher in vouchers]
+        print(tabulate(rows, headers, tablefmt="grid"))
