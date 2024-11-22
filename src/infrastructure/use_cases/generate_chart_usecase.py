cd3586dbfe9b6c6a3837e01d81d8cef33553f1a7
VicDario <26585356+VicDario@users.noreply.github.com>
2024-11-22 14:22:01 -0300

    fix: generate better image

--- src/infrastructure/use_cases/generate_chart_usecase.py
+++ src/infrastructure/use_cases/generate_chart_usecase.py
@@ -12,17 +12,18 @@ class GenerateChartUseCase:
         services = [row['service_name'] for row in data]
         profits = [row['total_profit'] for row in data]
         output_path="chart.png"
-        plt.figure(figsize=(8, 8))
+        plt.figure(figsize=(12, 12))
         plt.pie(
             profits,
             labels=services,
             autopct='%1.1f%%',
             startangle=140,
-            textprops={'fontsize': 10}
+            textprops={'fontsize': 10},
+
         )
-        plt.title(f"Ventas totales por servicio ({year})", fontsize=14)
+        plt.title(f"Ventas totales por servicio ({year})", fontsize=20,  y=1.05)
         plt.axis('equal')
-        plt.legend(title="Servicios", loc="best")
+        plt.subplots_adjust(left=0.2, right=0.8, top=0.9, bottom=0.1)
         plt.savefig(output_path, format="png", dpi=300)
         plt.close()
         print(f"Chart saved as {output_path}")
