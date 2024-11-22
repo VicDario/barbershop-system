51eb860209a5310c9350634c62b0ff030d39c85c
VicDario <26585356+VicDario@users.noreply.github.com>
2024-11-22 13:51:52 -0300

    fix: correct query

--- src/infrastructure/repositories/client_repository.py
+++ src/infrastructure/repositories/client_repository.py
@@ -8,14 +8,14 @@ class ClientRepository(ClientRepositoryInterface):
     def get_daily_clients_attended(self):
         return self.db.fetch_all("""
             SELECT
-                date,
+                b.date,
                 c.rut AS client_rut,
                 CONCAT(c.name, ' ', c.surname) AS client_name,
                 c.email AS client_email,
                 b.code AS booking_code,
                 b.shop_id AS shop_id
             FROM bookings b
-            JOIN clients c ON c.rut = b.clients_rut
+            JOIN clients c ON c.rut = b.client_rut
             JOIN attends a ON b.code = a.booking_code
             GROUP by date, c.rut, b.code, b.shop_id
             ORDER BY date;
