f81e43853b0397da15dc994c5fa9ccd333629d54
VicDario <26585356+VicDario@users.noreply.github.com>
2024-11-22 13:53:12 -0300

    fix: correct message

--- src/infrastructure/use_cases/daily_clients_attended_usecase.py
+++ src/infrastructure/use_cases/daily_clients_attended_usecase.py
@@ -7,7 +7,7 @@ class DailyClientsAttendedUseCase:
         
     def execute(self):
         daily_clients = self.repository.get_daily_clients_attended()
-        print(f"Clientes atendidos diariamente por Trabajador")
+        print(f"Clientes atendidos por dia")
         headers = ["Fecha", "Rut cliente", "Nombre cliente", "Email cliente", "Código reserva", "ID tienda"]
         rows = [[daily_client["date"], daily_client["client_rut"], daily_client["client_name"], daily_client["client_email"], daily_client["booking_code"], daily_client["shop_id"]] for daily_client in daily_clients]
         print(tabulate(rows, headers, tablefmt="grid"))
